4c91616243206b5b6f06ab70902d69ecf4f39074
Kieran <kieran.mcnamara@resmed.com>
2021-11-29 17:13:48 +0000

    Adding solution for day3 part1

---
+++ 2015/inc/day3/grid_traverser.h
@@ -0,0 +1,34 @@
+//
+// Created by kieran McNamara on 29/11/2021.
+//
+
+#ifndef ADVENTOFCODE_GRID_TRAVERSER_H
+#define ADVENTOFCODE_GRID_TRAVERSER_H
+
+#include <cstdint>
+#include <vector>
+#include <map>
+
+class GridTraverser {
+
+public:
+    GridTraverser();
+    GridTraverser(int32_t i32_startX, int32_t i32_startY);
+    void ResetState();
+    void ProcessInstructions(const std::string& str_instructions);
+    uint32_t GetNumHousesRecPresents();
+    uint32_t GetNumPresentsDelivered();
+    ~GridTraverser();
+
+protected:
+    static std::string GetKeyString(int32_t i32_x, int32_t i32_y);
+    void UpdateInternalFromInstruction(int32_t i32_x, int32_t i32_y);
+
+    int32_t i32_startX_;
+    int32_t i32_startY_;
+    uint32_t u32_numHousesRecPresents_;
+    uint32_t u32_numPresentsDelivered_;
+    std::map<std::string, uint32_t> map_housesPresents_;
+};
+
+#endif //ADVENTOFCODE_GRID_TRAVERSER_H
---
+++ 2015/test/day3/tests.cpp
@@ -0,0 +1,51 @@
+//
+// Created by kieran McNamara on 29/11/2021.
+//
+
+#include <gtest/gtest.h>
+#include <fstream>
+#include "../../inc/day3/grid_traverser.h"
+
+TEST(GridTraverser, SanityTests) {
+    auto traversal = GridTraverser();
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 0);
+    ASSERT_TRUE(traversal.GetNumPresentsDelivered() == 0);
+
+    traversal.ProcessInstructions(">");
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 2);
+
+    traversal.ResetState();
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 0);
+    ASSERT_TRUE(traversal.GetNumPresentsDelivered() == 0);
+
+    traversal.ProcessInstructions("^>v<");
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 4);
+
+    traversal.ResetState();
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 0);
+    ASSERT_TRUE(traversal.GetNumPresentsDelivered() == 0);
+
+    traversal.ProcessInstructions("^v^v^v^v^v");
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 2);
+}
+
+TEST(GridTraverser, InputTest) {
+    std::string str_inputInstructions;
+    std::ifstream file("../../../2015/test/day3/test_data.in.txt");
+    std::string str_line;
+
+    while (getline(file, str_line)) {
+        if (str_inputInstructions.empty()) {
+            str_inputInstructions = str_line;
+        } else {
+            str_inputInstructions += str_line;
+        }
+    }
+
+    auto traversal = GridTraverser();
+    ASSERT_TRUE(traversal.GetNumHousesRecPresents() == 0);
+    ASSERT_TRUE(traversal.GetNumPresentsDelivered() == 0);
+
+    traversal.ProcessInstructions(str_inputInstructions);
+    std::cout << std::endl << "Number houses received presents: " << traversal.GetNumHousesRecPresents() << std::endl;
+}
